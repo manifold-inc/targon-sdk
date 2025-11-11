2ceac3318f9e63f48127d79eecad8d9b765c8863
venishpatidar <venishpatidar@gmail.com>
2025-11-11 05:15:23 -0600

    fix: updating api entrypoints

--- src/targon/client/constants.py
+++ src/targon/client/constants.py
@@ -1,4 +1,4 @@
-DEFAULT_BASE_URL = "http://localhost:80"
+DEFAULT_BASE_URL = "https://api.beta.targon.com"
 API_VERSION = "v1"
 
 # API Endpoints
@@ -6,8 +6,8 @@ INVENTORY_ENDPOINT = f"/{API_VERSION}/capacity"
 DEPLOYMENT_ENDPOINT = f"/{API_VERSION}/serverless"
 
 # Heim Build Service
-HEIM_BASE_URL = "https://heim.185.209.179.245.nip.io"
-HEIM_BUILD_ENDPOINT = "/build"
+HEIM_BASE_URL = "https://api.beta.targon.com"
+HEIM_BUILD_ENDPOINT = f"{API_VERSION}/v1/heim/build"
 
 # Function Service
 FUNC_REG_ENDPOINT = "/" + API_VERSION + "/apps/{app_id}/functions"
