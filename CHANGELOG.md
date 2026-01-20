cbdfb5ba8ca69e35141e0e66b6079d72207f8786
venishpatidar <venishpatidar@gmail.com>
2026-01-20 11:17:07 -0600

    added h100s support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,4 +7,5 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [0.3.0] - 2026-01-15
+## [0.3.1] - 2026-01-20
+- Added the support for H100s gpus.
--- src/targon/runtime/runtime_init.py
+++ src/targon/runtime/runtime_init.py
@@ -105,15 +105,21 @@ class _StubCompute:
     CPU_XL = "cpu-xl"
 
     # GPU Tiers
+    # H200s
     H200_SMALL = "h200-small"
     H200_MEDIUM = "h200-medium"
     H200_LARGE = "h200-large"
     H200_XL = "h200-xl"
-
+    # H100s
+    H100_SMALL = "h100-small"
+    H100_MEDIUM = "h100-medium"
+    H100_LARGE = "h100-large"
+    H100_XL = "h100-xl"
+    # RTX4090s
     RTX4090_SMALL="rtx4090-small"
     RTX4090_MEDIUM="rtx4090-medium"
     RTX4090_LARGE="rtx4090-large"
-
+    
 def _stub_decorator(*args, **kwargs):
     def decorator(func):
         return func
